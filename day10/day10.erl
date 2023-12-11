001388adf9ed002173fc4875dd767c49fe48682f
Will Vining <wfv@vining.dev>
2023-12-10 19:29:33 -0700

    Day 10 Part 2
    With a pretty substantial hint

--- day10/day10.erl
+++ day10/day10.erl
@@ -40,6 +40,73 @@ get(X, Y, Array) ->
 set(X, Y, Value, Array) ->
     array:set(Y, array:set(X, Value, array:get(Y, Array)), Array).
 
+loop_mask(X, Y, Maze) ->
+    Visited =
+        array:new(
+          array:size(Maze),
+          {default, array:new(array:size(array:get(0, Maze)),
+                              {default, false})}),
+    {NewVisited, {NewX, NewY}} = step(X, Y, Maze, Visited),
+    {_, FinalVisited} = loop_length(NewX, NewY, Maze, NewVisited, 1),
+    FinalVisited.
+
+extract_loop(LoopMask, Maze) ->
+    Width = array:size(array:get(0, Maze)),
+    Height = array:size(Maze),
+    NewMaze = array:new(Height, {default, array:new(Width, {default, $.})}),
+    Coords = [{X, Y} || X <- lists:seq(0, Width - 1),
+                        Y <- lists:seq(0, Height - 1)],
+    F = fun ({X, Y}, Acc) ->
+                case get(X, Y, LoopMask) of
+                    true -> set(X, Y, get(X, Y, Maze), Acc);
+                    false -> Acc
+                end
+        end,
+    lists:foldl(F, NewMaze, Coords).
+
+count_interior(Loop) ->
+    Width = array:size(array:get(0, Loop)),
+    Height = array:size(Loop),
+    Coords = [{X, Y} || X <- lists:seq(1, Width - 2),
+                        Y <- lists:seq(1, Height - 2)],
+    Count = fun (true, Acc)  -> Acc + 1;
+                (false, Acc) -> Acc
+            end,
+    lists:foldl(
+      Count, 0,
+      [is_contained(X, Y, Loop) || {X, Y} <- Coords,
+                                   get(X, Y, Loop) =:= $.]).
+
+is_contained(X, Y, Loop) ->
+    Coords = [{X1, Y} || X1 <- lists:seq(0, X)],
+    CountCrossings = fun({X1, Y1}, Acc) ->
+                             case is_crossing(X1, Y1, Loop) of
+                                 true -> Acc + 1;
+                                 false -> Acc
+                             end
+                     end,
+    Crossings = lists:foldl(CountCrossings, 0, Coords),
+    odd(Crossings).
+
+is_crossing(X, Y, Loop) ->
+    C = get(X, Y, Loop),
+    lists:member(C, "JL|").
+
+odd(X) ->
+    (X rem 2) =:= 1.
+
+%% Replace the S tile with its actual value.
+replace_start(X, Y, Loop) ->
+    Candidates = [set(X, Y, C, Loop) || C <- "-7JLF|"],
+    F = fun(Arr) -> num_connections(X, Y, Arr) =:= 2 end,
+    hd(lists:filter(F, Candidates)).
+
+num_connections(X, Y, Loop) ->
+    Coords = [{X - 1, Y}, {X + 1, Y}, {X, Y - 1}, {X, Y + 1}],
+    Connections = [connected(X, Y, X1, Y1, Loop) || {X1, Y1} <- Coords],
+    lists:foldl(fun (true, Acc) -> Acc + 1; (_, Acc) -> Acc end,
+                0, Connections).
+
 loop_length(X, Y, Maze) ->
     Visited =
         array:new(
@@ -47,12 +114,12 @@ loop_length(X, Y, Maze) ->
           {default, array:new(array:size(array:get(0, Maze)),
                               {default, false})}),
     {NewVisited, {NewX, NewY}} = step(X, Y, Maze, Visited),
-    loop_length(NewX, NewY, Maze, NewVisited, 1).
+    element(1, loop_length(NewX, NewY, Maze, NewVisited, 1)).
 
 loop_length(X, Y, Maze, Visited, N) ->
     case get(X, Y, Maze) of
         $S ->
-            N;
+            {N, Visited};
         _ ->
             {NewVisited, {NewX, NewY}} = step(X, Y, Maze, Visited),
             loop_length(NewX, NewY, Maze, NewVisited, N + 1)
@@ -63,7 +130,7 @@ step(X, Y, Maze, Visited) ->
         [Next|_] ->
             {set(X, Y, true, Visited), Next};
         [] ->
-            {Visited, find_start(Maze)}
+            {set(X, Y, true, Visited), find_start(Maze)}
     end.
 
 next_step(X, Y, Maze, Visited) ->
